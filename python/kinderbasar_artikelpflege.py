a1653aa585f506b9efe12e1eb1e04934f7b58ab1
Joachim Wiemes <joachim.wiemes@targens.de>
2023-02-12 13:20:24 +0100

    Added command line arguments to latest script.

--- python/kinderbasar_artikelpflege.py
+++ python/kinderbasar_artikelpflege.py
@@ -5,15 +5,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 import time
+import sys
+import getopt
 
-USERNAME = "manuela.moedinger"
-PASSWORD = "cSbUUZVK9aK3:es"
-DATASOURCE_PATH = "/Volumes/Daten/Software-Projekte/Privat/scripts/python/artikel.csv"
+USERNAME = ""
+PASSWORD = ""
+DATASOURCE_PATH = ""
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(executable_path="/Users/joachim/chromedriver", chrome_options=chrome_options)
 driver.maximize_window()
+
+def readArgs(argv):
+  global USERNAME
+  global PASSWORD
+  global DATASOURCE_PATH
+
+  opts, args = getopt.getopt(argv,"hu:p:f:",["username=","password=","file="])
+  
+  for opt, arg in opts:
+    if opt == '-h':
+      print ('test.py -u <username> -p <password> -f <file>')
+      sys.exit()
+    elif opt in ("-u", "--username"):
+      USERNAME = arg
+    elif opt in ("-p", "--password"):
+      PASSWORD = arg
+    elif opt in ("-f", "--file"):
+      DATASOURCE_PATH = arg
 
 def login(url, driver):
   driver.get(url)
@@ -30,17 +50,21 @@ def createNewArticle(size, color, description, price):
   WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "article_submit")))
   driver.find_element(By.ID, "article_submit").click()
 
-# Einloggen
-login("https://www.easybasar.de/einloggen", driver)
+if __name__ == "__main__":
+  # Kommandozeilen-Argumente einlesen
+  readArgs(sys.argv[1:])
 
-# Skript pausieren bis Artikeleingabemaske geladen ist
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Größe")))
+  # Einloggen
+  login("https://www.easybasar.de/einloggen", driver)
 
-# Artikel erfassen
-with open(DATASOURCE_PATH, 'r') as file:
-    csvreader = csv.reader(file, delimiter=';')
-    header = next(csvreader)
-    for row in csvreader:
-      createNewArticle(row[1], row[2], row[0], row[3])
-      time.sleep(5)
+  # Skript pausieren bis Artikeleingabemaske geladen ist
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "Größe")))
+
+  # Artikel erfassen
+  with open(DATASOURCE_PATH, 'r') as file:
+      csvreader = csv.reader(file, delimiter=';')
+      header = next(csvreader)
+      for row in csvreader:
+        createNewArticle(row[1], row[2], row[0], row[3])
+        time.sleep(5)
